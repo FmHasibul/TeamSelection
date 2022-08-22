0b9bae825c233b91fda80b5b3559d34cb8dffd22
FmHasibul <fmhhasan@gmail.com>
2022-08-22 13:37:10 +0600

    calculation function done

--- playerSelection.js
+++ playerSelection.js
@@ -1,2 +1,29 @@
+// Getting input value function 
+function getInputValueById(getValue) {
+    const getInputValue = document.getElementById(getValue);
+    const valueString = getInputValue.value;
+    const integerValue = parseFloat(valueString);
+    return integerValue;
+}
 
+// Set Element 
+function setElementById(elementId, value) {
+    const grabElement = document.getElementById(elementId);
+    const getInneText = grabElement.innerText = value;
+    return getInneText;
+}
 
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const perPlayerCost = getInputValueById('per-player-cost')
+    const playersExpense = perPlayerCost *5
+    const totalPlayerCost = setElementById('players-cost', playersExpense)
+    
+})
+document.getElementById('total-expense-btn').addEventListener('click', function () {
+    const managerCost = getInputValueById('manager-cost');
+    
+    const coachCost = getInputValueById('coach-cost');
+    
+    const totalTeamExpense = managerCost + coachCost;
+    setElementById('total-expense', totalTeamExpense)
+})
